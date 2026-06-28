7cd7360b147fb337021f1f9540d946f2efad5817
314159otr <piotrbpersonal@gmail.com>
2026-06-28 13:18:04 +0200

    CH2: Message Brokers - L3: Connect

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const CONN_STRING string = "amqp://guest:guest@localhost:5672/"
+	con, err := amqp.Dial(CONN_STRING)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer con.Close()
+	fmt.Println("Connection was successful")
+
+	c := make(chan os.Signal)
+    signal.Notify(c, os.Interrupt)
+    <-c
+	fmt.Println("Shutting down")
 }
